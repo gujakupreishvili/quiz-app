43b44de4b53ac08c0a1c9790c6b9770566fd7228
gujajupreishvili <gujakupreishvili4@gmail.com>
2024-10-11 17:12:10 +0400

    fixed vercel bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-// import { useDarkmode } from "./context/mode"; // Ensure the path is correct
-
-import { Button, SwitchButton } from "./components/_molecules";
 import Header from "./components/header/header";
 import Main from "./components/main/main";
-
 export default function Home() {
-  // const { darkMode, toggleDarkmode } = useDarkmode(); // Use the custom hook here
-  // console.log(useDarkmode); // This should not be undefined
-
-
   return (
     <>
     <Header />
